56acfe35bd12b5b8efccc3149198ddb7307e6182
Emanuele <em.zaccaria@gmail.com>
2020-04-02 22:33:16 +0200

    Incredibile: Fine

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ $(document).ready(function(){
   var frecciaPrev = $(".prev-arrow");
   // selezioni l'elemento pallina
   var pallina = $(".balls i");
+  // gruppo di palline
+  var gruppoPalline = $("balls")
+  // gruppo di foto
+  var gruppoFoto = $("img")
 
 // quando clicci sulla freccia next
   frecciaNext.click(
@@ -30,26 +34,58 @@ $(document).ready(function(){
     }
   )
 // quando clicci sulla freccia frecciaPrev
-frecciaPrev.click(
-  function(){
-    // seleziono la foto visibile in quel momento
-    var visibleFoto = $(".fotos img.visible")
-    //
-    var selectedPallina = $(".balls i.selected");
-    //
-    if (visibleFoto.hasClass("first") === true) {
-      visibleFoto.removeClass("visible");
-      selectedPallina.removeClass("selected");
-      $(".last").addClass("visible");
-      $(".ultima").addClass("selected");
-    } else {
-      // faccio in modo che la foto scompaia
-      visibleFoto.removeClass("visible");
-      selectedPallina.removeClass("selected");
-      // rendo visibile la foto successiva
-      visibleFoto.prev().addClass("visible");
-      selectedPallina.prev().addClass("selected");
+  frecciaPrev.click(
+    function(){
+      // seleziono la foto visibile in quel momento
+      var visibleFoto = $(".fotos img.visible")
+      //
+      var selectedPallina = $(".balls i.selected");
+      //
+      if (visibleFoto.hasClass("first") === true) {
+        visibleFoto.removeClass("visible");
+        selectedPallina.removeClass("selected");
+        $(".last").addClass("visible");
+        $(".ultima").addClass("selected");
+      } else {
+        // faccio in modo che la foto scompaia
+        visibleFoto.removeClass("visible");
+        selectedPallina.removeClass("selected");
+        // rendo visibile la foto successiva
+        visibleFoto.prev().addClass("visible");
+        selectedPallina.prev().addClass("selected");
+      }
     }
-  }
-)
+  )
+// Quando clicchi sulla pallina si seleziona
+  pallina.click(
+    function(){
+      // seleziono la foto visibile in quel momento
+      var visibleFoto = $(".fotos img.visible")
+      // toglie il rosso a tutte le palline
+      pallina.removeClass("selected");
+      // mette il rosso alla pallina selezionata
+      $(this).addClass("selected");
+      //
+      if ($(this).hasClass("uno")) {
+        visibleFoto.removeClass("visible");
+        //
+        $(".fotos img.uno").addClass("visible");
+
+      } else if ($(this).hasClass("due")) {
+        visibleFoto.removeClass("visible");
+        //
+        $(".fotos img.due").addClass("visible");
+
+      } else if ($(this).hasClass("tre")) {
+        visibleFoto.removeClass("visible");
+        //
+        $(".fotos img.tre").addClass("visible");
+
+      } else if ($(this).hasClass("quattro")) {
+        visibleFoto.removeClass("visible");
+        // 
+        $(".fotos img.quattro").addClass("visible");
+      }
+    }
+  )
 });
