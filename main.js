10ccd2c6da6592eaf57cd5d5e8bb123153257a38
Emanuele <em.zaccaria@gmail.com>
2020-04-02 18:31:58 +0200

    JS Pallina rossa ultimata

--- main.js
+++ main.js
@@ -3,20 +3,29 @@ $(document).ready(function(){
   var frecciaNext = $(".next-arrow");
   // selezioniamo l'elemento freccia prev
   var frecciaPrev = $(".prev-arrow");
+  // selezioni l'elemento pallina
+  var pallina = $(".balls i");
 
 // quando clicci sulla freccia next
   frecciaNext.click(
     function(){
       // seleziono la foto visibile in quel momento
-      var visibleFoto = $(".fotos img.visible")
+      var visibleFoto = $(".fotos img.visible");
+      // selezioniamo la pallina visibile rossa
+      var selectedPallina = $(".balls i.selected");
+      // se viene selezionata l'ultima foto
       if (visibleFoto.hasClass("last") === true) {
         visibleFoto.removeClass("visible");
+        selectedPallina.removeClass("selected");
         $(".first").addClass("visible");
+        $(".prima").addClass("selected");
       } else {
-        // faccio in modo che la foto scompaia
+        // faccio in modo che la foto/red scompaia
         visibleFoto.removeClass("visible");
-        // rendo visibile la foto successiva
+        selectedPallina.removeClass("selected");
+        // rendo visibile la foto/red successiva
         visibleFoto.next().addClass("visible");
+        selectedPallina.next().addClass("selected");
       }
     }
   )
@@ -25,14 +34,21 @@ frecciaPrev.click(
   function(){
     // seleziono la foto visibile in quel momento
     var visibleFoto = $(".fotos img.visible")
+    //
+    var selectedPallina = $(".balls i.selected");
+    //
     if (visibleFoto.hasClass("first") === true) {
       visibleFoto.removeClass("visible");
+      selectedPallina.removeClass("selected");
       $(".last").addClass("visible");
+      $(".ultima").addClass("selected");
     } else {
       // faccio in modo che la foto scompaia
       visibleFoto.removeClass("visible");
+      selectedPallina.removeClass("selected");
       // rendo visibile la foto successiva
       visibleFoto.prev().addClass("visible");
+      selectedPallina.prev().addClass("selected");
     }
   }
 )
