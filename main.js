d407249b19d43226c90111425b102221bc48112f
Emanuele <em.zaccaria@gmail.com>
2020-04-02 17:17:37 +0200

    Accenni di JS

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-  
+  // selezioniamo l'elemento freccia next
+  var frecciaNext = $(".next-arrow");
+
+  frecciaNext.click(
+    function(){
+      // seleziono la foto visibile in quel momento
+      var visibleFoto = $(".fotos img.visible")
+      visibleFoto.removeClass("visible");
+      visibleFoto.addClass("notvisibile");
+    }
+  )
+
 });
