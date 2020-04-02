8f6b3f96be9d6b3accfdc7d8b4bbe6af6f3e6faf
Emanuele <em.zaccaria@gmail.com>
2020-04-02 17:49:28 +0200

    Click e cambio foto slider JS

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
 $(document).ready(function(){
   // selezioniamo l'elemento freccia next
   var frecciaNext = $(".next-arrow");
+  // selezioniamo l'elemento freccia prev
+  var frecciaPrev = $(".prev-arrow");
 
+// quando clicci sulla freccia next
   frecciaNext.click(
     function(){
       // seleziono la foto visibile in quel momento
       var visibleFoto = $(".fotos img.visible")
+      if (visibleFoto.hasClass("last") === true) {
+
+      } else {
+
+      }
+      // faccio in modo che la foto scompaia
       visibleFoto.removeClass("visible");
-      visibleFoto.addClass("notvisibile");
+      // rendo visibile la foto successiva
+      visibleFoto.next().addClass("visible");
     }
   )
+// quando clicci sulla freccia frecciaPrev
+frecciaPrev.click(
+  function(){
+    // seleziono la foto visibile in quel momento
+    var visibleFoto = $(".fotos img.visible")
+    // faccio in modo che la foto scompaia
+    visibleFoto.removeClass("visible");
+    // rendo visibile la foto successiva
+    visibleFoto.prev().addClass("visible");
+  }
+)
+
 
 });
